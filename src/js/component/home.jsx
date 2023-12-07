78f9181b18281b8e9a28c09b8768ef95a8e8bfec
Atulhom <joanguaschduran@gmail.com>
2023-12-07 19:18:37 +0000

    cambio

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,7 +4,20 @@ import Navbar from "./navbar";
 import Sort from "./sort";
 import Cards from "./cards";
 
-//create your first component
+//Los arrays de objetos van con {}
+//Los arrays normales van con [] - hay que buscar mas informacion sobre esto.
+// Practicar mapeos cona Array simples y Array de objetos.
+
+// Esto seria un mapeo al Array alumnos donde nos devuelve la propiedad nombre asoaciada al Item
+// alumnos.map(function (item, index, arr) {
+//   return `<li>${item.nombre}</li>`;
+// });
+
+/* trabajos de practica. Replicar el array de las navbar de la siguiente web: https://www.frontendmentor.io/challenges
+  que contiene las siguientes propiedades: Url(img), Title, Links(lenguajes), Parrafo. */
+
+// Info adicional - https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+
 const Home = () => {
   return (
     <div className="text-center">
